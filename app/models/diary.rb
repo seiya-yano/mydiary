c5830402b1114a62ea7f153992df2d2d4665edaa
seiya-yano <seyaseyaseiya@gmail.com>
2020-04-19 20:11:43 +0900

    ひとまず日記一覧画面完成

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,3 +1,15 @@
 class Diary < ApplicationRecord
   belongs_to :user
+  def self.search(search)
+    if search
+      Diaries.where(['title LIKE ?', "%#{search}%"])
+    else
+      Diaries.all
+    end
+  end
+
+  with_options presence: true do
+    validates :title, length: { maximum: 30 }
+    validates :text
+  end
 end
